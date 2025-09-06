9d59a9f5645221c2650d8c2c6bac5b5e23611c8d
Hetansh <hetanshsachaniya07@gmail.com>
2025-09-06 13:31:26 +0530

    feat(ui): setup Shadcn UI library and theme

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-
+import { Button } from './components/ui/button'
 function App() {
   return (
     <div className="flex items-center justify-center min-h-screen bg-black text-white">
-      <h1 className="text-4xl font-bold">TECH-VAULT</h1>
+      <h1 className="text-4xl font-bold p-4">TECH-VAULT</h1>
+      <Button>Get Started</Button>
     </div>
   )
 }
